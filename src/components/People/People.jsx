5b45250508dcb29a28d1b211d751bc68e7e72ade
Ibrahim Olawale <ibrahimolawale580@gmail.com>
2023-02-08 17:49:08 +0100

    switched back to BrowserRouter also used thead and tbody

--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -119,123 +119,142 @@ function People() {
             <div id = 'persons'>
                 <p>Persons</p>
                 <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>Action</th>
-                        <th>Action Date</th>
-                        <th>Gender</th>
-                        <th>Status</th>
-                        <th>Deceased</th>
-                        <th>Birth Place</th>
-                        <th>Address Line</th>
-                        <th>Address City</th>
-                        <th>Address Country</th>
-                        <th>Name Type</th>
-                        <th>Title Honourific</th>
-                        <th>First Name</th>
-                        <th>Middle Name</th>
-                        <th>SurName</th>
-                        <th>Maiden Name</th>
-                        <th>Other Name</th>
-                        <th>Option</th>
-                    </tr>
-                    {
-                       list.map(item =>(
-                            <tr key= {String(item['id'])}>
-                                <td>{item['id']}</td>
-                                <td>Add</td>
-                                <td>{item['Date_action'].replace(/\//g, '')}</td>
-                                <td>{item['gender']}</td>
-                                <td>Active</td>
-                                <td></td>
-                                <td>{item['Date_action']}</td>
-                                <td>{item['Address']}</td>
-                                <td>{item['Address_city']}</td>
-                                <td>{item['Country']}</td>
-                                <td></td>
-                                <td>{item['Title']}</td>
-                                <td>{item['first_name']}</td>
-                                <td>{item['Other_name']}</td>
-                                <td>{item['last_name']}</td>
-                                <td>{item['Maiden_name']}</td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                       )) 
-                    }
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Action</th>
+                            <th>Action Date</th>
+                            <th>Gender</th>
+                            <th>Status</th>
+                            <th>Deceased</th>
+                            <th>Birth Place</th>
+                            <th>Address Line</th>
+                            <th>Address City</th>
+                            <th>Address Country</th>
+                            <th>Name Type</th>
+                            <th>Title Honourific</th>
+                            <th>First Name</th>
+                            <th>Middle Name</th>
+                            <th>SurName</th>
+                            <th>Maiden Name</th>
+                            <th>Other Name</th>
+                            <th>Option</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                        list.map(item =>(
+                                <tr key= {String(item['id'])}>
+                                    <td>{item['id']}</td>
+                                    <td>Add</td>
+                                    <td>{item['Date_action'].replace(/\//g, '')}</td>
+                                    <td>{item['gender']}</td>
+                                    <td>Active</td>
+                                    <td></td>
+                                    <td>{item['Date_action']}</td>
+                                    <td>{item['Address']}</td>
+                                    <td>{item['Address_city']}</td>
+                                    <td>{item['Country']}</td>
+                                    <td></td>
+                                    <td>{item['Title']}</td>
+                                    <td>{item['first_name']}</td>
+                                    <td>{item['Other_name']}</td>
+                                    <td>{item['last_name']}</td>
+                                    <td>{item['Maiden_name']}</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                        )) 
+                        }
+                    </tbody>
                 </table>
             </div>
             <div id="numbers-description">
                 <div id="numbers">
                     <p>Numbers</p>
                     <table>
-                        <tr>
-                            <th>ID</th>
-                            <th>Doc Type</th>
-                            <th>Values</th>
-                        </tr>
-                        {
-                            list.map(item =>(
-                                <tr key={String(item.id)}>
-                                    <td>{item.id}</td>
-                                    <td>Voters Card</td>
-                                    <td>{item['Voters_card']}</td>
-                                </tr>
-                            ))
-                        }
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Doc Type</th>
+                                <th>Values</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                list.map(item =>(
+                                    <tr key={String(item.id)}>
+                                        <td>{item.id}</td>
+                                        <td>Voters Card</td>
+                                        <td>{item['Voters_card']}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
                     </table>
                 </div>
             
                 <div id="descriptions">
                     <p>Description</p>
                     <table>
-                    <tr>
-                        <th>id</th>
-                        <th>Descr ID</th>
-                        <th>Description</th>
-                   </tr>
-                    {
-                        list.map(item =>(
-                            <tr key= {String(item.id)}>
-                               <td>{item.id}</td> 
-                               <td>1</td>
-                               <td>Politically Exposed Person(PEP) </td>
-                            </tr>
-                        ))
-                    }
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>Descr ID</th>
+                            <th>Description</th>
+                        </tr>
+                   </thead>
+                   <tbody>
+                        {
+                            list.map(item =>(
+                                <tr key= {String(item.id)}>
+                                <td>{item.id}</td> 
+                                <td>1</td>
+                                <td>Politically Exposed Person(PEP) </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
                    </table>
                 </div>
                 <div id="countries">
                     <p>Countries</p>
                     <table>
-                       <tr>
-                        <th>ID</th>
-                        <th>Country type</th>
-                        <th>Name</th>
-                        <th>Rating</th>
-                        <th>PM code</th>
-                       </tr>
-                       {
-                        list.map(item =>(
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>Citizenship</td>
-                                <td>{item['Country']}</td>
-                                <td></td>
-                                <td>{item.Country.slice(0,2)}</td>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Country type</th>
+                                <th>Name</th>
+                                <th>Rating</th>
+                                <th>PM code</th>
                             </tr>
-                        ))
-                       }
+                        </thead>
+                        <tbody>
+                            {
+                                list.map(item =>(
+                                    <tr key={item.id}>
+                                        <td>{item.id}</td>
+                                        <td>Citizenship</td>
+                                        <td>{item['Country']}</td>
+                                        <td></td>
+                                        <td>{item.Country.slice(0,2)}</td>
+                                    </tr>
+                                ))
+                            }
+                       </tbody>
                     </table>
                 </div>
                 <div id= "date">
                     <p>Date</p>
                     <table>
-                        <tr>
-                            <th>id</th>
-                            <th>Date type</th>
-                            <th>Date Pm</th>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <th>id</th>
+                                <th>Date type</th>
+                                <th>Date Pm</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                         {
                             list.map(item =>(
                                 <tr key = {item.id}>
@@ -245,6 +264,7 @@ function People() {
                                 </tr>
                             ))
                         }
+                        </tbody>
                     </table>
                 </div>
             </div>
@@ -252,64 +272,73 @@ function People() {
                 <div id="sanctions">
                     <p>Sanctions</p>
                     <table>
-                        <tr>
-                            <th>Id</th>
-                            <th>list Code</th>
-                            <th>Date start</th>
-                            <th>Date stop</th>
-                            <th>S-name</th>
-                            <th>Status</th>
-                            <th>RT</th>
-                            <th>Descr</th>
-                            <th>Descr1</th>
-                        </tr>
-                        {
-                            list.map(item =>(
-                                <tr key= {item.id}>
-                                    <td>{item.id}</td>
-                                    <td>PEP</td>
-                                    <td>{item['Date_action'].replace(/\//g,'')}</td>
-                                    <td>{item['Date_action'].replace(/\//g,'')}</td>
-                                    <td>RU</td>
-                                    <td>Active</td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                            ))
-                        }
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>list Code</th>
+                                <th>Date start</th>
+                                <th>Date stop</th>
+                                <th>S-name</th>
+                                <th>Status</th>
+                                <th>RT</th>
+                                <th>Descr</th>
+                                <th>Descr1</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                list.map(item =>(
+                                    <tr key= {item.id}>
+                                        <td>{item.id}</td>
+                                        <td>PEP</td>
+                                        <td>{item['Date_action'].replace(/\//g,'')}</td>
+                                        <td>{item['Date_action'].replace(/\//g,'')}</td>
+                                        <td>RU</td>
+                                        <td>Active</td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
                     </table>
                 </div>
                 <div id = 'roles'>
                         <p>Roles</p>
                         <table>
-                            <tr>
-                                <th>id</th>
-                                <th>Name</th>
-                                <th>Role type</th>
-                                <th>Start Date</th>
-                                <th>Stop Date</th>
-                                <th>Code</th>
-                                <th>Title</th>
-                            </tr>
-                            {
-                                list.map((item, index) =>(
-                                    <tr key={item.id}>
-                                        <td>{item.id}</td>
-                                        <td></td>
-                                        <td>{item['Roles']}</td>
-                                        <td>{item['Date_action'].replace(/\//g,'')}</td>
-                                        <td></td>
-                                        <td>{index+1}</td>
-                                        <td>{item['Roles']}</td>
-                                    </tr>
-                                ))
-                            }
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>Name</th>
+                                    <th>Role type</th>
+                                    <th>Start Date</th>
+                                    <th>Stop Date</th>
+                                    <th>Code</th>
+                                    <th>Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    list.map((item, index) =>(
+                                        <tr key={item.id}>
+                                            <td>{item.id}</td>
+                                            <td></td>
+                                            <td>{item['Roles']}</td>
+                                            <td>{item['Date_action'].replace(/\//g,'')}</td>
+                                            <td></td>
+                                            <td>{index+1}</td>
+                                            <td>{item['Roles']}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
                         </table>
                 </div>
             </div>
             <div id = 'associate'>
                 <p>Associates</p>
                 <table>
+                    <thead>
                     <tr>
                         <th>Id</th>
                         <th>Assoc Id</th>
@@ -333,6 +362,7 @@ function People() {
                         <th>Suffix</th>
                         <th>Options</th>
                     </tr>
+                    </thead>
                 </table>
             </div>
         </section>
